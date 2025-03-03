510ebf01909255228bb5b8ecd145b3829562a0cf
shajipr21 <shajipr21@gmail.com>
2025-03-03 15:03:24 +0530

    terms and conditions added

--- resources/views/admin/terms_and_conditions.blade.php
+++ resources/views/admin/terms_and_conditions.blade.php
@@ -21,12 +21,254 @@
 
 
             <!-- Latest Leads -->
-            <div class="row">
+          <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-														
-                            <iframe src="{{url('agreement/agreement-1-10-2024.pdf')}}#toolbar=0&navpanes=0" style="width:80%;height:700px;"></iframe>
+						sasdsD SADSADSADSAASDAS
+						
+						<h2>Getlead CRM Partner Program</h2>
+						<p>Document Version 1.0</p>
+						<p>Last Updated : 01-01-2024</p>
+						
+						<h4>Introduction</h4>
+						<p>Getlead Analytics Private Limited, hereinafter referred to as "Company," is the creator
+						and provider of Getlead CRM, a cutting-edge Software as a Service (SAAS) solution
+						designed to empower small and medium-scale businesses by streamlining customer
+						relationship management. At the core of our mission is the commitment to assist
+						businesses in optimizing their customer interactions, fostering growth, and
+						achieving sustained success.</p>
+
+
+						<h4>Product Overview</h4>
+						<p>Getlead CRM is a comprehensive customer relationship management
+						solution that offers a wide array of functionalities tailored to meet the diverse
+						needs of businesses. It serves as a centralized hub for managing customer
+						information, sales processes, and marketing efforts.</p>
+						<h4>Key Features</h4>
+						<p><b>Lead Management:</b> Getlead CRM excels in lead management, providing tools
+						for capturing, organizing, and nurturing leads throughout their lifecycle. It
+						facilitates efficient lead tracking and conversion.</p>
+						<p><b>Contact Management:</b> Our CRM offers robust contact management
+						capabilities, enabling businesses to maintain up-to-date and organized
+						customer databases.</p>
+
+						<p><b>Sales Pipeline:</b> Getlead CRM visualizes the sales journey through an intuitive
+						sales pipeline. This aids in monitoring opportunities, tracking progress, and
+						closing deals effectively.</p>
+						<p><b>Task and Calendar Management:</b> The CRM includes task and calendar
+						management features, simplifying the scheduling of appointments,
+						follow-ups, and other important activities.</p>
+						<p><b>Reporting and Analytics:</b> Businesses can derive actionable insights from
+						their data with our reporting and analytics tools. It provides valuable metrics
+						for informed decision-making.</p>
+						<p><b>Customization:</b> Getlead CRM is highly customizable, allowing businesses to
+						tailor it to their specific needs, workflows, and industry requirements. This
+						adaptability ensures a seamless fit for a variety of businesses.</p>
+
+						<h4>Benefits for Partners</h4>
+						<p>Partnering with Getlead CRM offers a range of advantages, including:</p>
+						<p><b>Market-Proven Solution:</b> Gain access to a CRM solution that has
+						demonstrated success in the market, with a track record of delivering results
+						for businesses.</p>
+						<p><b>Recurring Revenue:</b> Partners have the opportunity to generate recurring
+						revenue through sales, support, and subscription-based models.</p>
+						<p><b>Training and Certification:</b> We provide comprehensive training and
+						certification programs to empower partners with the knowledge and skills
+						needed to excel in promoting and implementing Getlead CRM.</p>
+						<p><b>Marketing Support:</b> Partners can leverage our marketing support, including
+						co-branding opportunities, marketing collateral, and lead generation
+						assistance.</p>
+
+
+						<h4>Target Audience</h4>
+						<p>Getlead CRM is ideally suited for small and medium-scale businesses across
+						various industries. It caters to businesses looking to enhance their customer
+						relationship management, sales, and marketing efforts for improved
+						efficiency and growth.</p>
+						<h4>Use Cases</h4>
+						<p>Our CRM has been successfully implemented in numerous scenarios,
+						including:</p>
+
+						<p>Streamlining sales processes and increasing conversion rates.
+						Improving customer engagement and satisfaction through effective
+						communication.</p>
+						<p>Enhancing marketing strategies with data-driven insights.
+						Simplifying lead tracking and nurturing for better lead-to-customer
+						conversion.</p>
+
+						<h4>Technical Requirements:</h4>
+						<p>To fully utilize Getlead CRM, partners and users should ensure the following technical
+						requirements are met:</p>
+						<p>Reliable internet connectivity.</p>
+						<p>Compatible web browsers and devices as specified in the system
+						requirements documentation.</p>
+
+
+						<h4>Future Developments</h4>
+						<p>Our commitment to innovation means that Getlead CRM will continue to
+						evolve. We are actively working on future updates and enhancements to
+						provide partners and users with even more valuable features and capabilities.
+						Stay tuned for our product roadmap updates</p>
+
+						<hr>
+
+
+						<h3 class="mt-4">Pricing and Subscription Plans</h3>
+						<p><b>Cost of Getlead CRM:</b> ₹6999/user/year.</p>
+						<p><b>Subscription Options: </b>Customers can opt for quarterly, half-yearly, or yearly
+						subscriptions.</p>
+						<p><b>Discounts:</b> Special discounts are available for those selecting the quarterly,
+						half-yearly, and annual payment plans.</p>
+						<p><b>Training and Implementation Charges :</b> Amount Equal to Three Months of
+						Subscription</p>
+						<p><b>Training Charges Additional :</b> ₹1500/Hour</p>
+						<p><b>Annual Payments Perks:</b> Free implementation and 5 hours of
+						complementary training (to be availed within a year).</p>
+						<p><b>Training & Implementation: </b>All training and implementation support will be provided online.</p>
+						<p><b>Onsite Training & Implementation:</b> Available at additional charges</p>
+
+						<hr>
+											
+						<h3 class="mt-4">Reseller Program Options</h3>
+						<p><b>Referral Partner: </b>As a Referral Partner, you will introduce businesses to
+						Getlead CRM.</p>
+						<p><b>Implementation Partner:</b> As an Implementation Partner, you’ll assist
+						businesses in setting up and optimizing their use of Getlead CRM.</p>
+
+						<hr>
+
+						<h3 class="mt-4 mb-3">Revenue Sharing and Commissions</h3>
+
+						<h5>Commission Structure:</h5>
+
+						<p>The revenue-sharing model for resellers and implementation partners will be
+						outlined in this section. Commissions are typically calculated based on the
+						sales performance and activities of the partner.</p>
+
+						<h5>Sales Commission:</h5>
+
+						<p>Partners may earn a commission for each sale of Getlead CRM licenses or
+						subscriptions. The commission rate will be specified in this document and
+						may vary based on factors such as the partner's sales volume and
+						performance.</p>
+
+						<h5>Subscription-Based Revenue Sharing:</h5>
+						<p>In cases where Getlead CRM is offered on a subscription basis, partners may
+						receive recurring commissions for as long as their referred customers
+						maintain active subscriptions. This offers the potential for continuous revenue
+						generation.</p>
+
+						<h5>Performance Bonuses:</h5>
+
+						<p>To incentivize exceptional performance, partners may have the opportunity to
+						earn performance bonuses based on achieving specific sales milestones or
+						targets. The criteria for earning these bonuses will be clearly defined.
+						Commission Payments and Payout Procedures</p>
+
+						<h3 class="mt-4">Commission Structure:</h3>
+
+						<p>As a valued partner in the Getlead Partner Program, you are eligible to receive
+						commissions on sales of Getlead CRM services and products. Commissions
+						are earned when a customer, referred by you, completes a payment for
+						Getlead services. The commission for each sale will be credited to your
+						account following the successful processing of the customer's payment.</p>
+
+						<h5>Payment Timeline:</h5>
+						<p>To ensure proper accounting and processing, payments of commissions will
+						be released 30 days after the subscription payment has been received and
+						processed by Getlead. This 30-day period allows for necessary verifications
+						and ensures compliance with financial regulations.</p>
+
+						<h5>Minimum Payout Amount:</h5>
+
+						<p>To streamline the payment process, the minimum payout amount for
+						commissions is set at INR 3000. This threshold must be met or exceeded in
+						your account before a payout request can be processed. This policy is in place
+						to ensure efficient handling of financial transactions and administrative ease.</p>
+
+						<h5>Payout Requests:</h5>
+
+						<p>As a participant in the Getlead Partner Program, you can submit requests for
+						payment of your accumulated commissions through our dedicated partner
+						portal. This portal provides a convenient and secure way for you to manage
+						your commissions, view your earnings, and request payouts. To request a
+						payout, simply log in to the partner portal, verify your earnings, and if the
+						minimum payout amount is met, submit a payout request following the
+						instructions provided.</p>
+
+						<h5>Payment Method and Currency:</h5>
+
+						<p>All commission payments will be made in the specified currency and through
+						the payment method chosen by you at the time of registering for the Getlead
+						Partner Program. We offer various payment methods to accommodate your
+						preferences. Please ensure your payment details are accurate and up-to-date
+						in the partner portal to avoid any delays in receiving your commissions</p>
+
+
+
+						<h4 class="mt-4">Tracking and Reporting:</h4>
+
+						<p>The Company will provide partners with a tracking and reporting system to
+						monitor their sales activities, commissions earned, and bonuses achieved.
+						Regular reports will be made available to partners.</p>
+
+						<h4 class="mt-4">Taxation and Withholding:</h4>
+
+						<p>Partners are responsible for any taxation or withholding requirements related
+						to commissions. The Company may withhold taxes as required by applicable
+						laws and regulations.</p>
+
+						<h4 class="mt-4">Audit Rights:</h4>
+
+						<p>The Company reserves the right to audit partner accounts and commission
+						calculations to ensure accuracy and compliance with the partnership
+						agreement. Partners are expected to cooperate with any audit requests.</p>
+
+
+						<h4 class="mt-4">Dispute Resolution:</h4>
+						<p>Procedures for resolving disputes related to commissions or revenue sharing
+						should be clearly outlined. This may involve mediation, arbitration, or another
+						agreed-upon mechanism for dispute resolution.</p>
+						<h4 class="mt-4">Termination and Commission Payments:</h4>
+						<p>The partnership agreement should specify the treatment of commissions and
+						bonuses in the event of partnership termination. Commissions earned before
+						termination will typically be paid out according to the agreed-upon schedule.</p>
+						<h4 class="mt-4">Changes to Commission Structure:</h4>
+						<p>The Company reserves the right to make changes to the commission
+						structure, rates, or criteria for bonuses. However, any significant changes will
+						be communicated to partners with reasonable notice.</p>
+						<h4 class="mt-4">Non-Disclosure of Commission Details:</h4>
+						<p>Partners are expected to maintain the confidentiality of commission details,
+						including rates and payment structures, to protect sensitive financial
+						information.</p>
+
+
+						<h4 class="mt-4">Commissions and Benefits</h4>
+						<p>Below is the table detailing commissions based on sales volume:</p>
+						<table width="100%" border=1 cellspacing=0 cellpadding=0 >
+						<tr ><th>Monthly Sales Amount (INR)</th><th>Referral Partner Commission</th><th>Implementation Partner Commission</th><th>Renewal Commission (Year 2 Onwards)</th></tr>
+						<tr ><td>Up to 50,000</td><td> 10%</td><td>25% + Implementation charges </td><td>5%<td></tr>
+						<tr ><td>50,001 - 2 Lakh</td><td> 15%</td><td>28% + Implementation charges</td><td>5%</td></tr>
+						<tr ><td>Above 2 Lakh</td><td> 20%</td><td>30% + Implementation charges</td><td>5%</td></tr>
+						</table>
+
+						<h4 class="mt-4">Support & Communication</h4>
+						<p>Disputes: For any disputes or concerns, please get in touch at
+						support@getleadcrm.com.</p>
+						<p><b>Feature Updates & Training:</b> Partners will be kept informed about new
+						feature releases, and training sessions will be arranged.</p>
+						<p><b>Dedicated Partner Manager: </b>An exclusive manager will be at your disposal to
+						assist you.</p>
+						<p><b>Partner Portal:</b> A dedicated space to keep track of payouts, referral details,
+						and updates.</p>
+						<p>Thank you for choosing to be a part of the Getlead CRM family. Together, we'll
+						ensure businesses reach new heights.</p>
+						<p><b>Warm Regards,<b></p>
+						<p><b>Team Getlead CRM.</b></p>
+
+						
+                            <!--<iframe src="{{url('agreement/agreement-1-10-2024.pdf')}}#toolbar=0&navpanes=0" style="width:80%;height:700px;"></iframe>-->
 
                         </div>
                     </div>
