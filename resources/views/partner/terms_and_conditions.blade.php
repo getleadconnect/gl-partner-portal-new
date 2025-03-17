dac5e2283b718cab9ef7144c805a71d9802bf86e
shajipr21 <shajipr21@gmail.com>
2025-03-17 13:16:43 +0530

    payment changes

--- resources/views/partner/terms_and_conditions.blade.php
+++ resources/views/partner/terms_and_conditions.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.master')
+@extends('partner.master')
 @section('content')
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
